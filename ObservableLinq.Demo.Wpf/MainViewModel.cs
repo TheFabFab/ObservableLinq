ea312820227ae425b362d9fad9c0d9ba580d9cfa
Csaba Fabian <csaba_fabian@hotmail.com>
2014-02-26 17:59:45 +0100

    Added demo app with non-functional reorder listbox.

---
+++ ObservableLinq.Demo.Wpf/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableLinq.Demo.Wpf
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<int> _collection;
+
+        public MainViewModel()
+        {
+            _collection = new ObservableCollection<int>(new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+        }
+
+        public ObservableCollection<int> Collection
+        {
+            get { return _collection; }
+        }
+    }
+}
